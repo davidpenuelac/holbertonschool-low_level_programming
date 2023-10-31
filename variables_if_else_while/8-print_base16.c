ecdba724806dc2c9ff82aaac7557870972b5f768
davidpenuelac <6937@holbertonstudents.com>
2023-10-30 18:14:13 -0700

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 48;
+int c = 97;
+
+while (a <= 57)
+{
+putchar(a);
+a++;
+}
+while (c <= 102)
+{
+putchar(c);
+c++;
+}
+putchar(10);
+return (0);
+}
