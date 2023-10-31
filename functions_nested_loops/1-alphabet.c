c94bf106c5e312ff7fd7382fa8421ce6354df432
davidpenuelac <6937@holbertonstudents.com>
2023-10-31 09:43:34 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet -  prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int c = 97
+
+while (c <= 122)
+{
+_putchar(c);
+c++;
+}
+_putchar(10);
+}
