195b5f9bff73c45e4d683803d7b27645ba8f2a8f
davidpenuelac <6937@holbertonstudents.com>
2023-10-31 18:35:58 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,15 +16,18 @@ for (a = 0; a < 10; a++)
 for (b = 0; b < 10; b++)
 {
 c = a * b;
-if (c >= 10)
-_putchar((c / 10) + '0');
-_putchar((c % 10 + '0'));
-if (b == 9)
+if (b == 0)
+{
+_putchar('0');
 continue;
+}
 _putchar(',');
 _putchar(' ');
 if (c < 10)
 _putchar (' ');
+if (c >= 10)
+_putchar((c / 10) + '0');
+_putchar((c % 10 + '0'));
 }
 _putchar('\n');
 }
