667295a9d9ece3562719397292968c6481b4326d
davidpenuelac <6937@holbertonstudents.com>
2023-11-07 09:57:39 -0800

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+int l = 0;
+
+while (str[l] != '\0')
+{
+if (l % 2 == 0)
+_putchar(str[l]);
+l++;
+}
+_putchar('\n');
+}
