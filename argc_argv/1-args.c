c03997bc7c5463a7de56861fac33a6f09e1640db
davidpenuelac <6937@holbertonstudents.com>
2023-11-15 18:26:40 -0800

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - check the code
+* @argc: the number of argument
+* @argv: double pointer
+*
+* Return: Always 0.
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+int i = argc - 1;
+printf("%d\n", i);
+return (0);
+}
