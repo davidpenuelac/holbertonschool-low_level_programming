38fb083e4c48d91021e8408ed388f82f7df938b2
davidpenuelac <6937@holbertonstudents.com>
2023-11-15 18:43:29 -0800

    2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - check the code
+* @argc: number of arguments
+* @argv: arguments
+*
+* Return: Always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
