cdd9135fbca7b9685caff7933131d71776d0b85b
davidpenuelac <6937@holbertonstudents.com>
2023-11-15 18:20:58 -0800

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - check the code
+* @argc: the len of the string
+* @argv: pointer of an array
+*
+* Return: always 0.
+*/
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
