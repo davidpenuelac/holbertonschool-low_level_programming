1e722ad4a689d98399cc4ef5b882f88269acdaa6
davidpenuelac <6937@holbertonstudents.com>
2023-11-23 08:54:40 -0800

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+* struct dog - Short description
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+* Description: Longer description
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
