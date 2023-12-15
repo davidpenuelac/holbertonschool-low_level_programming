e5a07d628666559b6cd3fd2ab1d21227a9c53b8a
davidpenuelac <6937@holbertonstudents.com>
2023-12-14 21:04:27 -0800

    1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * dlistint_len - Returns the number of elements in a dlistint_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
