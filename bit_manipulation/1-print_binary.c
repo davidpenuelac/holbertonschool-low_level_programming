a2ac4db25ba561b8b344d55d033789aa969d218f
davidpenuelac <6937@holbertonstudents.com>
2023-12-15 14:28:26 -0800

    1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
 
@@ -10,15 +11,7 @@
 
 void print_binary(unsigned long int n)
 {
-	int s = sizeof(unsigned long int) * 8;
-	int i = 1 << (s - 1);
-
-	while (i > 0)
-	{
-		if (n & i)
-			_putchar('1');
-		else
-			_putchar('0');
-		i >>= 1;
-	}
+	unsigned i;
+	for (i = 1 << 31; i > 0; i = i / 2)
+		(n & i) ? _putchar('1') : _putchar('0');
 }
