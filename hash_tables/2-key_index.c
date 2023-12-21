754a06d06567282fc6d970f9120910bf1a0004a7
davidpenuelac <6937@holbertonstudents.com>
2023-12-21 12:04:45 -0800

    2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - Calculates the index at which a key/value pair should be stored
+ * @key: The key
+ * @size: The size of the array of the hash table
+ *
+ * Return: The index at which the key/value pair should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hv = hash_djb2(key);
+	unsigned long int i = hash_value % size;
+
+	return (i);
+}
