b449b9871bb4b00bb298687b9243f570582b98eb
davidpenuelac <6937@holbertonstudents.com>
2023-12-21 17:38:50 -0800

    4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: The hash table to look into
+ * @key: The key to look for
+ *
+ * Return: The value associated with the key, or NULL if wrong
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i;
+	hash_node_t *c;
+
+	if (!ht || !key || *key == '\0')
+	{
+		return (NULL);
+	i = key_index((unsigned char *)key, ht->size);
+	c = ht->array[i];
+	while (c)
+	{
+		if (strcmp(c->key, key) == 0)
+			return (c->value);
+		c = c->next;
+	}
+
+	return (NULL);
+}
