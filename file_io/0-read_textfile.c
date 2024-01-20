353690158fb1a21a79bb57acd1e91c829226614d
davidpenuelac <6937@holbertonstudents.com>
2024-01-20 14:59:39 -0800

    0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -18,31 +18,31 @@ int file_descriptor;
 char *buffer;
 ssize_t bytes_read, bytes_written;
 if (filename == NULL)
-return 0;
+return (0);
 file_descriptor = open(filename, O_RDONLY);
 if (file_descriptor == -1)
-return 0;
+return (0);
 buffer = malloc(letters);
 if (buffer == NULL)
 {
 close(file_descriptor);
-return 0;
+return (0);
 }
 bytes_read = read(file_descriptor, buffer, letters);
 if (bytes_read == -1)
 {
 free(buffer);
 close(file_descriptor);
-return 0;
+return (0);
 }
 bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
 free(buffer);
 close(file_descriptor);
-return 0;
+return (0);
 }
 free(buffer);
 close(file_descriptor);
-return bytes_written;
+return (bytes_written);
 }
